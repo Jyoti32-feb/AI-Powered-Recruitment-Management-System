1ecddf4f49586dbd2890189fa10819a19bac783e
Jyoti Shrinivas Pathade <83999132+Jyoti32-feb@users.noreply.github.com>
2026-09-05 08:05:24 +0530

    Add User entity and repository

---
+++ src/main/java/com/jyoti/ai_recruitment_management/entity/Role.java
@@ -0,0 +1,7 @@
+package com.jyoti.ai_recruitment_management.entity;
+
+public enum Role {
+    ADMIN,
+    RECRUITER,
+    CANDIDATE
+}
---
+++ src/main/java/com/jyoti/ai_recruitment_management/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.jyoti.ai_recruitment_management.repository;
+
+import com.jyoti.ai_recruitment_management.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
