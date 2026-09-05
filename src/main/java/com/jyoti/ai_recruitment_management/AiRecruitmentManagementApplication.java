893d3b4dfdd9db8f6496a83c6ab44f9502635dcb
Jyoti Shrinivas Pathade <83999132+Jyoti32-feb@users.noreply.github.com>
2026-09-05 07:47:24 +0530

    Initial Spring Boot setup and project structure

---
+++ src/main/java/com/jyoti/ai_recruitment_management/AiRecruitmentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.jyoti.ai_recruitment_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRecruitmentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiRecruitmentManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jyoti/ai_recruitment_management/AiRecruitmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyoti.ai_recruitment_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRecruitmentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
